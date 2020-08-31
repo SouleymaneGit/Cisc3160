d81ac1e888b12da671b56a8da34a01a260dd2b05
Souleymane <39134972+SouleymaneGit@users.noreply.github.com>
2020-08-31 12:41:29 -0400

    Add files via upload

---
+++ Helloword/src/Helloword.java
@@ -0,0 +1,24 @@
+/* Name : Thierno Souleymane Diallo
+ * Class: Cisc115
+ * assigment 0
+ * date due: 09/3/2019
+ * date Submit:08/28/2019
+ * this program displays at the screen two sentence 
+ * 1st : Hello World 
+ * 2nd : Thierno Souleymane Diallo
+ */
+public class Helloword {
+
+    public static void main(String[] args) {
+       System.out.print("Hello World!\n");
+       System.out.println("Thierno Souleymane Diallo");
+	}
+
+}
+
+/* program output
+
+Hello World!
+Thierno Souleymane Diallo
+
+*/
